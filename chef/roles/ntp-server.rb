e1d3409aa72b91e23ec314eaaf5a704dd8d9ed2b
Victor Lowther <victor.lowther@gmail.com>
2013-09-03 13:29:20 -0500

    Fix up NTP barclamp to be CB 2.0 compatible and deployable.

--- chef/roles/ntp-server.rb
+++ chef/roles/ntp-server.rb
@@ -4,6 +4,6 @@ description "NTP Servier Role - NTP master for the cloud"
 run_list(
          "recipe[ntp]"
 )
-default_attributes "ntp" => { "external_servers" => [], "config" => { "environment" => "ntp-base-config" } }
+default_attributes()
 override_attributes()
 
