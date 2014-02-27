987615902a132decec68979eba359cef8a7aea77
Thomas Boerger <tboerger@suse.de>
2014-02-27 11:22:05 +0100

    Added magic utf8 token to ruby files

--- chef/data_bags/crowbar/migrate/ntp/001_drop_admin_ip_eval.rb
+++ chef/data_bags/crowbar/migrate/ntp/001_drop_admin_ip_eval.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 def upgrade ta, td, a, d
   a.delete 'admin_ip_eval'
   return a, d
--- chef/roles/ntp-client.rb
+++ chef/roles/ntp-client.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 name "ntp-client"
 description "NTP Client Role - NTP client for the cloud"
 run_list(
